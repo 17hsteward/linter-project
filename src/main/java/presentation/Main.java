20cca74400a3ab7800a08ac60ae7abf6ca27b36c
rhit-xuj8 <78224422+rhit-xuj8@users.noreply.github.com>
2023-05-04 08:48:44 -0400

    Filechooser

--- src/main/java/presentation/Main.java
+++ src/main/java/presentation/Main.java
@@ -1,8 +1,25 @@
 package presentation;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.filechooser.FileSystemView;
+import java.io.File;
+import java.io.IOException;
+
+
 public class Main {
 
     public static void main(String[] args) {
 
+        JFileChooser chooser = new JFileChooser();
+        chooser.setMultiSelectionEnabled(true);
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Java Files", "java");
+        chooser.setFileFilter(filter);
+        chooser.showOpenDialog(null);
+
+        File[] files = chooser.getSelectedFiles();
+
+
     }
+
 }
